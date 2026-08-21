13b71ebb147337967858e318a4e4d5096cf80be4
aly ahmed <ananyaly9@gmail.com>
2026-08-21 23:26:28 +0300

    Cast PORT to number and update listen address

--- NULL/backend_prj/src/server.ts
+++ NULL/backend_prj/src/server.ts
@@ -4,11 +4,11 @@ dotenv.config();
 import app from './app';
 import { connectDB } from './config/database';
 
-const PORT = process.env.PORT || 5000;
+const PORT = (process.env.PORT || 5000) as number;
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
   });
